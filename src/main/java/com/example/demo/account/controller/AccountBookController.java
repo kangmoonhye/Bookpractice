64fc8409d213c2af74a41bfd12a9ffa787f7040b
kangmoonhye <ansgp0323@naver.com>
2026-05-13 14:55:18 +0900

    [FEAT] cors

--- src/main/java/com/example/demo/account/controller/AccountBookController.java
+++ src/main/java/com/example/demo/account/controller/AccountBookController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/boards/{boardIdx}/account-books")
 @RequiredArgsConstructor
